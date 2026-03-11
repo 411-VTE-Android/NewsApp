05cc93024d9f45ca2789b293b0f1dc925f50bd27
VicTavares8 <vte80988@lasallebajio.edu.mx>
2026-03-11 02:56:33 -0600

    Creación y primera versión del modelo de datos 'News'

---
+++ app/src/main/java/com/example/newsapp/models/News.kt
@@ -0,0 +1,23 @@
+package com.example.newsapp.models
+
+import android.icu.text.CaseMap
+
+data class News (
+    val id : Int,
+    val title : String,
+    val date : String,
+    val imageUrl : String
+)
+
+val breakingNewsList = listOf(
+    News(1, "El presidente de EE.UU no muestra signos de arrepentimiento", "08/02/2024", ""),
+    News(2, "Bañarse en la piscina del desierto de cleopatra", "09/02/2024", "")
+)
+
+val worlwideNewsList = listOf(
+    News(1, "El presidente de EE.UU no muestra signos de arrepentimiento", "", ""),
+    News(2, "Bañarse en la piscina del desierto de cleopatra", "", ""),
+    News(3, "Gigantes tecnológicos y el uso de la IA", "", ""),
+    News(4, "El rover de marte envía fotos impactantes", "", "")
+
+)
