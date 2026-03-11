e2cb01082b553f82984b4cd27c048353035ffb8b
VicTavares8 <vte80988@lasallebajio.edu.mx>
2026-03-11 02:02:56 -0600

    Definición de la paleta de colores / Creación de los paquetes 'components' y 'models'

--- app/src/main/java/com/example/newsapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/newsapp/ui/theme/Color.kt
@@ -9,3 +9,8 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val PrimaryPurple = Color(0xFF6352FF)
+val InnactiveTextGrey = Color(0xFFBDBDBD)
+val CardBackgroundGrey = Color(0xFFE0E0E0)
+val DarkText = Color(0xFF1A1A1A)
