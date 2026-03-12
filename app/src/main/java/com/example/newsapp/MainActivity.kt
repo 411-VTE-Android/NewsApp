2b5326d8950036523200256fd58802bb6f1f8058
VicTavares8 <vte80988@lasallebajio.edu.mx>
2026-03-11 23:20:43 -0600

    Implementación de los componentes en el MainActivity

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.newsapp.components.Tabs
+import com.example.newsapp.components.TopBar
+import com.example.newsapp.components.breakingNewsCarrousel
+import com.example.newsapp.components.worldwideNewsSection
+import com.example.newsapp.models.breakingNewsList
+import com.example.newsapp.models.worlwideNewsList
 import com.example.newsapp.ui.theme.NewsAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +30,25 @@ class MainActivity : ComponentActivity() {
         setContent {
             NewsAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
+                    ) {
+                        TopBar()
+                        Spacer(modifier = Modifier
+                            .height(8.dp))
+
+                        Tabs()
+                        Spacer(modifier = Modifier
+                            .height(16.dp))
+
+                        breakingNewsCarrousel(newsList = breakingNewsList)
+                        Spacer(modifier = Modifier
+                            .height(16.dp))
+
+                        worldwideNewsSection(newsList = worlwideNewsList)
+                    }
                 }
             }
         }
--- app/src/main/java/com/example/newsapp/models/News.kt
+++ app/src/main/java/com/example/newsapp/models/News.kt
@@ -11,13 +11,23 @@ data class News (
 
 val breakingNewsList = listOf(
     News(1, "El presidente de EE.UU no muestra signos de arrepentimiento", "08/02/2024", ""),
-    News(2, "Bañarse en la piscina del desierto de cleopatra", "09/02/2024", "")
+    News(2, "Bañarse en la piscina del desierto de cleopatra", "09/02/2024", ""),
+    News(3, "Nuevo avance en computación cuántica revoluciona la seguridad", "11/03/2026", ""),
+    News(4, "Descubren exoplaneta con condiciones similares a la Tierra", "10/03/2026", ""),
+    News(5, "Bolsas mundiales cierran al alza tras nuevos acuerdos comerciales", "10/03/2026", ""),
+    News(6, "Lanzamiento exitoso del nuevo satélite de observación climática", "09/03/2026", ""),
+    News(7, "Desarrollan tratamiento prometedor para enfermedades autoinmunes", "08/03/2026", "")
 )
 
 val worlwideNewsList = listOf(
-    News(1, "El presidente de EE.UU no muestra signos de arrepentimiento", "", "https://images.unsplash.com/photo-1585829365295-ab7cd400c167?q=80&w=500"),
-    News(2, "Bañarse en la piscina del desierto de cleopatra", "", ""),
-    News(3, "Gigantes tecnológicos y el uso de la IA", "", ""),
-    News(4, "El rover de marte envía fotos impactantes", "", "")
+    News(1, "El presidente de EE.UU no muestra signos de arrepentimiento", "", "https://cdn.pixabay.com/photo/2016/10/18/18/35/dementia-1750965_1280.jpg"),
+    News(2, "Bañarse en la piscina del desierto de cleopatra", "", "https://cdn.pixabay.com/photo/2024/03/21/23/15/ai-generated-8648646_1280.jpg"),
+    News(3, "Gigantes tecnológicos y el uso de la IA", "", "https://cdn.pixabay.com/photo/2023/10/30/05/02/ai-generated-8351759_1280.png"),
+    News(4, "El rover de marte envía fotos impactantes", "", "https://cdn.pixabay.com/photo/2026/02/26/18/56/mv-fotos-spacecraft-10145647_1280.png"),
+    News(5, "Trenes de levitación magnética de nueva generación llegan a Europa", "", ""),
+    News(6, "El renacimiento de la arquitectura sostenible en las grandes metrópolis", "", ""),
+    News(7, "Misión al fondo del océano revela especies marinas nunca antes vistas", "", ""),
+    News(8, "Histórico festival cultural en Japón rompe récord de asistencia", "", ""),
+    News(9, "Cumbre global del clima establece nuevas metas de energía limpia para 2030", "", "")
 
 )
